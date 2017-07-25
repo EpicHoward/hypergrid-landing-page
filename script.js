1fe92219c0574e447c54f48ce10dda588f90d78e
Eric R. Howard <ehoward@hudsonhs.com>
2017-07-25 13:48:16 +0000

    update

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-
-snippet ife
-    if (${1:true}) {
-        ${2}
-    } else {
-        ${0}
-    }
-
-snippet ife
-    if (${1:true}) {
-        ${2}
-    } else {
-        ${0}
-    }
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
